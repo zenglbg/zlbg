47ba2b96c7ec35cf4683e5f91965e4383b7df847
Your Name <you@example.com>
2025-03-11 17:09:04 +0200

    处理包错

--- src/app/NoSSRWrapper.tsx
+++ src/app/NoSSRWrapper.tsx
@@ -1,8 +1,14 @@
 import dynamic from 'next/dynamic'
-import React from 'react' 
-const NoSSRWrapper = props => ( 
-    <React.Fragment>{props.children}</React.Fragment> 
-) 
-export default dynamic(() => Promise.resolve(NoSSRWrapper), { 
-    ssr: false 
+import React from 'react'
+
+interface NoSSRWrapperProps {
+  children: React.ReactNode
+}
+
+const NoSSRWrapper = (props: NoSSRWrapperProps) => (
+  <React.Fragment>{props.children}</React.Fragment>
+)
+
+export default dynamic(() => Promise.resolve(NoSSRWrapper), {
+  ssr: false
 })

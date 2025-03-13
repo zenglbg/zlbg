72e8d0a1264c5d703ccdb0bfc29a765892898dda
Your Name <you@example.com>
2025-03-13 17:45:40 +0200

    response.headers.set('Access-Control-Allow-Origin', origin)

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,16 @@ export function middleware(request: NextRequest) {
   const response = NextResponse.next()
 
   // Add CORS headers
-  response.headers.set('Access-Control-Allow-Origin', 'https://unpkg.com https://hm.baidu.com https://www.google-analytics.com')
+  const allowedOrigins = ['https://unpkg.com', 'https://hm.baidu.com', 'https://www.google-analytics.com', 'https://www.zlbg.cc', 'https://zlbg.cc'];
+  const origin = request.headers.get('origin')
+
+  if (origin && allowedOrigins.includes(origin)) {
+    response.headers.set('Access-Control-Allow-Origin', origin)
+  }
+
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type')
+  response.headers.set('Access-Control-Allow-Credentials', 'true')
 
   return response
 }
